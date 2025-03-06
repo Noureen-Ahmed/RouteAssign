c8cb3ec857aa85fe4246a0d549d07ee9147276c1
noureen <noureenahmed1610@gmail.com>
2025-03-07 01:20:28 +0200

    start frame work uploaded

---
+++ Start Frame Work/src/components/Contact/Contact.jsx
@@ -0,0 +1,53 @@
+﻿/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import Style from './Contact.module.css';
+import Heading from '../Heading/Heading';
+export default function Contact() {
+    const [counter, setCounter] = useState();
+    useEffect(() => { }, [])
+    return (
+        <>
+                <Heading text={'Contact Component'} />
+
+            <div className="max-w-lg mx-auto p-6 text-left">
+                <form>
+                    <div className="mb-10">
+                        <input
+                            type="text"
+                            placeholder="userName"
+                            className="w-full border-b border-gray-300 focus:outline-none"
+                        />
+                    </div>
+                    <div className="mb-10">
+                        <input
+                            type="number"
+                            placeholder="userAge"
+                            className="w-full border-b border-gray-300 focus:outline-none"
+                        />
+                    </div>
+                    <div className="mb-10">
+                        <input
+                            type="email"
+                            placeholder="userEmail"
+                            className="w-full border-b border-gray-300 focus:outline-none"
+                        />
+                    </div>
+                    <div className="mb-10">
+                        <input
+                            type="password"
+                            placeholder="userPassword"
+                            className="w-full border-b border-gray-300 focus:outline-none"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="bg-teal-500 text-white py-2 px-4 rounded-md "
+                    >
+                        send Message
+                    </button>
+                </form>
+            </div>
+        </>
+    )
+}
+
---
+++ Start Frame Work/src/components/Heading/Heading.jsx
@@ -0,0 +1,21 @@
+﻿import React from "react";
+import { useLocation } from "react-router-dom";
+import'../../../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+const StartFrameworkHeading = ({ text }) => {
+    const { pathname } = useLocation();
+    const textColor = pathname === '/' || pathname === '/about' ? 'text-white' : 'text-[#2C3E50]';
+    const borderColor = pathname === '/' || pathname === '/about' ? 'border-white' : 'border-[#2C3E50]';
+
+    return (
+        <div className="font-bold text-2xl px-6 rounded-md flex flex-col items-center justify-center">
+            <h1 className={`${textColor} uppercase sm:text-4xl md:text-5xl `}>{text}</h1>
+            <div className="flex items-center gap-2 mt-2">
+                <div className={`w-16 border-t-4 ${borderColor}`}></div>
+                <span className={`${textColor} text-2xl `}>★</span>
+                <div className={`w-16 border-t-4 ${borderColor}`}></div>
+            </div>
+        </div>
+    );
+};
+
+export default StartFrameworkHeading;
